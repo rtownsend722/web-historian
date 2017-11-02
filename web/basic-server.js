1c06463de369b7e51c00393920435a16013005f7
rtownsend722 <rebecca.townsend722@gmail.com>
2017-11-01 20:40:41 -0700

    GET and POST html

--- web/basic-server.js
+++ web/basic-server.js
@@ -13,7 +13,7 @@ var server = http.createServer(handler.handleRequest);
 if (module.parent) {
   module.exports = server;
 } else {
-  server.listen(port, ip);
   console.log('Listening on http://' + ip + ':' + port);
+  server.listen(port, ip);
 }
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,60 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('../web/http-helpers.js');
+var fs = require('fs');
+
 // require more modules/folders here!
+var headers = httpHelpers.headers;
+var statusCode = 200;
+var response = '';
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === 'POST') {
+    var website = '';
+    req.on('data', function(chunk) {
+      website += chunk;
+    }).on('end', function() {
+      fs.readFile('./archives/sites.txt', function(err, data) {
+        if (err) { 
+          throw err; 
+        }
+
+        if (data.indexOf(website) >= 0) {
+          console.log('FOUND IT!!!!!!!');
+          //return the cached website
+          res.writeHead(statusCode, headers);
+          res.end(response);
+
+        } else {
+          fs.appendFile('./archives/sites.txt', website + ' '); // added url to queue
+          fs.readFile('./web/public/loading.html', 'utf-8', function(err, content) {
+            console.log(content);
+            if (err) { 
+              throw err; 
+            } else {
+              statusCode = 201;
+              res.writeHead(statusCode, headers);
+              res.write(content);
+              res.end();
+            }
+          });
+        }
+      });
+    });
+  } 
+  
+  if (req.method === 'GET') {
+    fs.readFile('./web/public/index.html', 'utf8', function(err, content) {
+      if (err) { 
+        throw err; 
+      } else {
+        statusCode = 200;
+        res.writeHead(statusCode, headers);
+        res.write(content);
+        res.end();
+      }      
+    });
+  }
+
+  
 };
