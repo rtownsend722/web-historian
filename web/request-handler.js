f54ace8f3f5d2e455a8af284a842787e0082ef48
rtownsend722 <rebecca.townsend722@gmail.com>
2017-11-02 14:43:16 -0700

    Render html files through get request and partially archive files

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,11 +2,13 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var httpHelpers = require('../web/http-helpers.js');
 var fs = require('fs');
+var http = require('http');
 
 // require more modules/folders here!
 var headers = httpHelpers.headers;
 var statusCode = 200;
 var response = '';
+
 
 exports.handleRequest = function (req, res) {
   if (req.method === 'POST') {
@@ -14,35 +16,44 @@ exports.handleRequest = function (req, res) {
     req.on('data', function(chunk) {
       website += chunk;
     }).on('end', function() {
-      fs.readFile('./archives/sites.txt', function(err, data) {
-        if (err) { 
-          throw err; 
-        }
+      //If website is cached, render website to client
+      if ('./archives/sites/' + website === true) {
+        fs.readFile('./archives/sites/' + website, 'utf8', function(err, content) {
+          if (err) { 
+            console.log('error encountered in rendering cached site'); 
+          } else {
+            statusCode = 200;
+            res.writeHead(statusCode, headers);
+            res.write(content);
+            res.end();
+          }      
+        });
 
-        if (data.indexOf(website) >= 0) {
-          console.log('FOUND IT!!!!!!!');
-          //return the cached website
-          res.writeHead(statusCode, headers);
-          res.end(response);
+      // If website is not cached, add website to sites.txt
+      } else {
+        fs.appendFile('./archives/sites.txt', website + ', ', function(err) {
+          if (err) {
+            console.log('error encountered adding site to sites.txt');
+          }
+        }); 
 
-        } else {
-          fs.appendFile('./archives/sites.txt', website + ' '); // added url to queue
-          fs.readFile('./web/public/loading.html', 'utf-8', function(err, content) {
-            console.log(content);
-            if (err) { 
-              throw err; 
-            } else {
-              statusCode = 201;
-              res.writeHead(statusCode, headers);
-              res.write(content);
-              res.end();
-            }
-          });
-        }
-      });
+        //also render loading to client
+        fs.readFile('./web/public/loading.html', 'utf-8', function(err, content) {
+          if (err) { 
+            console.log('error encountered rendering loading page'); 
+          } else {
+            statusCode = 201;
+            res.writeHead(statusCode, headers);
+            res.write(content);
+            res.end();
+          }
+        });
+      }
     });
+
   } 
   
+  //render initial page
   if (req.method === 'GET') {
     fs.readFile('./web/public/index.html', 'utf8', function(err, content) {
       if (err) { 
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,21 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+// Look in archive sites.txt for files that haven't been cached
+
+var http = require('http');
+var fs = require('fs');
+
+//make queue array
+var queue;
+fs.readFile('./archives/sites.txt', 'utf-8', function(err, content) {
+  queue = content.split(',');
+});
+
+for (var i = 0; i < queue.length; i++) {
+//for each in queue
+  var file = fs.createWriteStream('./archives/test.html');
+  var request = http.get('www.google.com', function(response) {
+    response.pipe(file);
+  });
+}
+//clear sites.txt
